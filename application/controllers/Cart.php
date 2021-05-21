5e4eff1041e08b42866445ce12f00ecdcd7321e5
sailaxmi501 <sailaxmibaratam501@gmail.com>
2021-05-21 21:35:47 +0530

    Update Cart.php

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -1,102 +1,86 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Cart extends CI_Controller {
-     
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-		public function __construct() 
-		{
+			<?php 
+			defined('BASEPATH') OR exit('No direct script access allowed');
+			class Cart extends CI_Controller {
+				public function __construct() 
+				{
 
-			parent::__construct();
-			$this->load->model(array('defaultmodel'));  
-		}
-		public function index()
-		{	
-			 
-			$data['cart_list']=$this->defaultmodel->getAllCartList();
-			$cart_details=$this->session->set_userdata('cartdetails',$data['cart_list']);
-	        $this->load->view('cart',$data);
-		}
-
-		public function cartDelete()
-	    {    
-		    $cart_id=base64_decode($this->uri->segment(3));
-	        $condition=array('cart_id'=>$cart_id);
-		    $result=$this->defaultmodel->simpleDelete(CART,$condition);
-		    if($result >0) {
-			$this->session->set_flashdata('msg', 'Item Deleted Successfully');
-			redirect('cart');  
-		    } else {
-		    	$this->session->set_flashdata('msg', 'Item Delete Failed');
-		    	redirect('cart');
-		    }
-	    }
-
-	    public function checkout()
-	    {
-	 	    $cart_details=$this->session->userdata('cartdetails');
-	 	    $check['title'] = "checkout";
-	 	    $this->form_validation->set_rules('name', 'Name', 'required');
-	 	    $this->form_validation->set_rules('email', 'Email', 'required');
-	 	    $this->form_validation->set_rules('address', 'Address', 'required');
-	 	    $this->form_validation->set_rules('phone', 'Phone', 'required');
-
- 	        $this->form_validation->set_error_delimiters('<span class="error">', '</span>');
-            if ($this->form_validation->run() == FALSE)
-            {
-	 	    $this->load->view('checkout',$check);
-	        }else{
-	 	    $created_date =date('Y-m-d H:i:s');
-					$postdata=array(
-					    'name' => $this->input->post('name'),
-					    'email'=> $this->input->post('email'),
-					    'phone'=> $this->input->post('phone'),
-					    'address'=>$this->input->post('address'),
-						'created_date'=>$created_date
-					);
-			$upd_status=$this->defaultmodel->simpleInsert(ADDRESSES,$postdata);
+					parent::__construct();
+					$this->load->model(array('defaultmodel'));  
+				}
+				public function index()
+				{	
 					
-			if($upd_status >0) {
-					$product_id=array();
-					foreach ($cart_details as $key) {
-						$product_id[]=$key['product_id'];
+					$data['cart_list']=$this->defaultmodel->getAllCartList();
+					$cart_details=$this->session->set_userdata('cartdetails',$data['cart_list']);
+					$this->load->view('cart',$data);
+				}
 
+				public function cartDelete()
+				{    
+					$cart_id=base64_decode($this->uri->segment(3));
+					$condition=array('cart_id'=>$cart_id);
+					$result=$this->defaultmodel->simpleDelete(CART,$condition);
+					if($result >0) {
+						$this->session->set_flashdata('msg', 'Item Deleted Successfully');
+						redirect('cart');  
+					} else {
+						$this->session->set_flashdata('msg', 'Item Delete Failed');
+						redirect('cart');
+					}
+				}
+
+				public function checkout()
+				{
+					$cart_details=$this->session->userdata('cartdetails');
+					$check['title'] = "checkout";
+					$this->form_validation->set_rules('name', 'Name', 'required');
+					$this->form_validation->set_rules('email', 'Email', 'required');
+					$this->form_validation->set_rules('address', 'Address', 'required');
+					$this->form_validation->set_rules('phone', 'Phone', 'required');
+
+					$this->form_validation->set_error_delimiters('<span class="error">', '</span>');
+					if ($this->form_validation->run() == FALSE)
+					{
+						$this->load->view('checkout',$check);
+					}else{
+						$created_date =date('Y-m-d H:i:s');
+						$postdata=array(
+							'name' => $this->input->post('name'),
+							'email'=> $this->input->post('email'),
+							'phone'=> $this->input->post('phone'),
+							'address'=>$this->input->post('address'),
+							'created_date'=>$created_date
+						);
+						$upd_status=$this->defaultmodel->simpleInsert(ADDRESSES,$postdata);
+						
+						if($upd_status >0) {
+							$product_id=array();
+							foreach ($cart_details as $key) {
+								$product_id[]=$key['product_id'];
+
+							}
+							$pro_id=implode(",", $product_id);
+							$orderdata=array(
+								'address_id'=>$upd_status,
+								'order_date'=>$created_date,
+								'product_id'=>$pro_id	
+							);
+							$orders=$this->defaultmodel->simpleInsert(ORDERS,$orderdata);
+							if($orders>0){
+								$this->session->unset_userdata('cartdetails');	
+								$this->db->truncate(CART);
+								$this->session->set_flashdata('msg', 'Order Submitted Successfully');
+								redirect('orderSuccess');  
+							}
+						}else {
+							$this->session->set_flashdata('msg', 'Order Submitted Failed');
+							redirect('checkout');
+						}
+					}
+				}
+				public function orderSuccess()
+				{
+					
+					$this->load->view('success');
+				} 
 			}
-			$pro_id=implode(",", $product_id);
-			$orderdata=array(
-						'address_id'=>$upd_status,
-						'order_date'=>$created_date,
-						'product_id'=>$pro_id	
-					);
-			$orders=$this->defaultmodel->simpleInsert(ORDERS,$orderdata);
-			if($orders>0){
-			       $this->session->unset_userdata('cartdetails');	
-			       $this->db->truncate(CART);
-		           $this->session->set_flashdata('msg', 'Order Submitted Successfully');
-		           redirect('orderSuccess');  
-		    }
-			}else {
-				   $this->session->set_flashdata('msg', 'Order Submitted Failed');
-				   redirect('checkout');
-			       }
-		    }
-	    }
-	    public function orderSuccess()
-	    {
-	    		
-	    	$this->load->view('success');
-	    } 
-        }
