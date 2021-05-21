8ce8ed5490c889bf0b8d34d2bade152ebae7c0ff
sailaxmi501 <84580340+sailaxmi501@users.noreply.github.com>
2021-05-21 21:21:18 +0530

    Update Cart.php

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -2,37 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Cart extends CI_Controller {
      
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 		public function __construct() 
 		{
 
 			parent::__construct();
 			$this->load->model(array('defaultmodel'));  
 		}
+		//Cart data
 		public function index()
-		{	
-			 
+		{ 
 			$data['cart_list']=$this->defaultmodel->getAllCartList();
 			$cart_details=$this->session->set_userdata('cartdetails',$data['cart_list']);
 	        $this->load->view('cart',$data);
 		}
-
+		//Delete cart items
 		public function cartDelete()
-	    {    
+	        {    
 		    $cart_id=base64_decode($this->uri->segment(3));
 	        $condition=array('cart_id'=>$cart_id);
 		    $result=$this->defaultmodel->simpleDelete(CART,$condition);
@@ -43,8 +28,8 @@ class Cart extends CI_Controller {
 		    	$this->session->set_flashdata('msg', 'Item Delete Failed');
 		    	redirect('cart');
 		    }
-	    }
-
+	      }
+		//Add Address & Checkout the order 
 	    public function checkout()
 	    {
 	 	    $cart_details=$this->session->userdata('cartdetails');
@@ -94,6 +79,7 @@ class Cart extends CI_Controller {
 			       }
 		    }
 	    }
+	    //Order Success
 	    public function orderSuccess()
 	    {
 	    		
