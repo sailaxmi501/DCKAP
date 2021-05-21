112d32bd6a9ee40bab35df8caecb56d018de9067
sailaxmi501 <sailaxmibaratam501@gmail.com>
2021-05-21 22:45:51 +0530

    code format

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dckap
- task
+Product List Page
+The list will have Product Name, Image, Price, Short Description
+The product can be Add to the Cart
+
+Checkout Page
+On the Checkout page, Products which has been added to the cart can be viewed.
+After reviewing can able to add the Customer Details
+Name
+Email
+Phone Number
+Address
+Once Click Place Order Button order should be placed. Order details need to be stored in DB
--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -1,88 +1,92 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Cart extends CI_Controller {
      
-		public function __construct() 
-		{
+	public function __construct() 
+	{
+		parent::__construct();
+		$this->load->model(array('defaultmodel'));  
+	}
 
-			parent::__construct();
-			$this->load->model(array('defaultmodel'));  
-		}
-		//Cart data
-		public function index()
-		{ 
-			$data['cart_list']=$this->defaultmodel->getAllCartList();
-			$cart_details=$this->session->set_userdata('cartdetails',$data['cart_list']);
-	        $this->load->view('cart',$data);
-		}
-		//Delete cart items
-		public function cartDelete()
-	        {    
-		    $cart_id=base64_decode($this->uri->segment(3));
-	        $condition=array('cart_id'=>$cart_id);
-		    $result=$this->defaultmodel->simpleDelete(CART,$condition);
-		    if($result >0) {
+	//Cart data
+	public function index()
+	{ 
+		$data['cart_list']=$this->defaultmodel->getAllCartList();
+		$cart_details=$this->session->set_userdata('cartdetails',$data['cart_list']);
+        $this->load->view('cart',$data);
+	}
+
+	//Delete cart items
+	public function cartDelete()
+    {    
+	    $cart_id=base64_decode($this->uri->segment(3));
+        $condition=array('cart_id'=>$cart_id);
+	    $result=$this->defaultmodel->simpleDelete(CART,$condition);
+	    if ($result >0) {
 			$this->session->set_flashdata('msg', 'Item Deleted Successfully');
 			redirect('cart');  
-		    } else {
-		    	$this->session->set_flashdata('msg', 'Item Delete Failed');
-		    	redirect('cart');
-		    }
-	      }
-		//Add Address & Checkout the order 
-	    public function checkout()
-	    {
-	 	    $cart_details=$this->session->userdata('cartdetails');
-	 	    $check['title'] = "checkout";
-	 	    $this->form_validation->set_rules('name', 'Name', 'required');
-	 	    $this->form_validation->set_rules('email', 'Email', 'required');
-	 	    $this->form_validation->set_rules('address', 'Address', 'required');
-	 	    $this->form_validation->set_rules('phone', 'Phone', 'required');
+	    } else {
+	    	$this->session->set_flashdata('msg', 'Item Delete Failed');
+	    	redirect('cart');
+	    }
+    }
 
- 	        $this->form_validation->set_error_delimiters('<span class="error">', '</span>');
-            if ($this->form_validation->run() == FALSE)
-            {
-	 	    $this->load->view('checkout',$check);
-	        }else{
-	 	    $created_date =date('Y-m-d H:i:s');
-					$postdata=array(
-					    'name' => $this->input->post('name'),
-					    'email'=> $this->input->post('email'),
-					    'phone'=> $this->input->post('phone'),
-					    'address'=>$this->input->post('address'),
-						'created_date'=>$created_date
-					);
+	//Add Address & Checkout the order 
+    public function checkout()
+    {
+ 	    $cart_details=$this->session->userdata('cartdetails');
+ 	    $check['title'] = "checkout";
+
+ 	    $this->form_validation->set_rules('name', 'Name', 'required');
+ 	    $this->form_validation->set_rules('email', 'Email', 'required');
+ 	    $this->form_validation->set_rules('address', 'Address', 'required');
+ 	    $this->form_validation->set_rules('phone', 'Phone', 'required');
+	    $this->form_validation->set_error_delimiters('<span class="error">', '</span>');
+
+        if ($this->form_validation->run() == FALSE)
+        {
+ 	    	$this->load->view('checkout',$check);
+        } else {
+ 	    	$created_date =date('Y-m-d H:i:s');
+			$postdata=array(
+			    'name' => $this->input->post('name'),
+			    'email'=> $this->input->post('email'),
+			    'phone'=> $this->input->post('phone'),
+			    'address'=>$this->input->post('address'),
+				'created_date'=>$created_date
+			);
 			$upd_status=$this->defaultmodel->simpleInsert(ADDRESSES,$postdata);
-					
+				
 			if($upd_status >0) {
-					$product_id=array();
-					foreach ($cart_details as $key) {
-						$product_id[]=$key['product_id'];
+				$product_id=array();
+				foreach ($cart_details as $key) {
+					$product_id[]=$key['product_id'];
+				}
 
-			}
-			$pro_id=implode(",", $product_id);
-			$orderdata=array(
-						'address_id'=>$upd_status,
-						'order_date'=>$created_date,
-						'product_id'=>$pro_id	
-					);
-			$orders=$this->defaultmodel->simpleInsert(ORDERS,$orderdata);
-			if($orders>0){
-			       $this->session->unset_userdata('cartdetails');	
-			       $this->db->truncate(CART);
-		           $this->session->set_flashdata('msg', 'Order Submitted Successfully');
-		           redirect('orderSuccess');  
-		    }
-			}else {
-				   $this->session->set_flashdata('msg', 'Order Submitted Failed');
-				   redirect('checkout');
-			       }
+				$pro_id=implode(",", $product_id);
+				$orderdata=array(
+							'address_id'=>$upd_status,
+							'order_date'=>$created_date,
+							'product_id'=>$pro_id	
+						);
+				$orders=$this->defaultmodel->simpleInsert(ORDERS,$orderdata);
+				if ($orders>0) {
+		       		$this->session->unset_userdata('cartdetails');	
+		       		$this->db->truncate(CART);
+	           		$this->session->set_flashdata('msg', 'Order Submitted Successfully');
+	           		redirect('orderSuccess');  
+	    		}
+			} else {
+			   $this->session->set_flashdata('msg', 'Order Submitted Failed');
+			   redirect('checkout');
 		    }
 	    }
-	    //Order Success
-	    public function orderSuccess()
-	    {
-	    		
-	    	$this->load->view('success');
-	    } 
-        }
+    }
+
+    //Order Success
+    public function orderSuccess()
+    {		
+    	$this->load->view('success');
+    } 
+}
